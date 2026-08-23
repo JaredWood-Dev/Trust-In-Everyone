4f07ac4ce11623dda3fbb15c20a95de1e831e6c0
Jared <jared.wood002@gmail.com>
2026-08-23 18:16:27 -0500

    added basic health component

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+    public int regen;
+    
+    [NonSerialized]
+    public HashSet<DamageTypes> Resistances = new HashSet<DamageTypes>();
+    [SerializeField]
+    public List<DamageTypes> DamageResistances = new List<DamageTypes>();
+
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        
+        //Copy data from out serialized field
+        Resistances.Clear();
+        foreach (var type in DamageResistances)
+        {
+            Resistances.Add(type);            
+        }
+        
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    
+    public void ChangeHealth(int changeHealth)
+    {
+        if (health + changeHealth > maxHealth)
+            health = maxHealth;
+        else if (health + changeHealth < 0)
+        {
+            //TODO: REPLACE LATER
+            Destroy(gameObject);
+        }
+        else
+        {
+            health += changeHealth;
+        }
+    }
+    
+    public void Damage(int damage, DamageTypes damageType, Vector2 knockback)
+    {
+        //If resistant, take half damage
+        if (Resistances.Contains(damageType))
+        {
+            ChangeHealth(-(damage / 2));
+        }
+        else
+        {
+            ChangeHealth(-damage);
+        }
+
+        if (_rb != null)
+        {
+            _rb.AddForce(knockback, ForceMode2D.Impulse);
+        }
+    }
+}
